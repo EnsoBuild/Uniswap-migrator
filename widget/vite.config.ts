c12bd7d5b137a0bfa49e61148bb02b2a82357249
pixelis <zhadkov@gmail.com>
2025-04-30 15:25:28 +0200

    vite config

---
+++ widget/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    lib: {
+      entry: "./src/index.tsx", // Entry point for your component
+      name: "UniswapMigrator", // Global name for UMD builds
+      fileName: (format) => `uniswap-migrator.${format}.js`, // Output file naming
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "wagmi",
+        "viem",
+        "@tanstack/react-query",
+      ], // Mark React as a peer dependency
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
